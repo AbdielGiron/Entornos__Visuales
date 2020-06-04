9d13aaa826a4f1f38a50144de8514687a3827a45
Rene Enamorado <Rene Enamorado@DESKTOP-TSL1SID>
2020-06-03 18:27:19 -0600

    Funcion String

--- IPARCIAL/Trabajos de Clase/FuncionesString/Form1.Designer.vb
+++ IPARCIAL/Trabajos de Clase/FuncionesString/Form1.Designer.vb
@@ -22,17 +22,228 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtCadena = New System.Windows.Forms.TextBox()
+        Me.txtLongitud = New System.Windows.Forms.TextBox()
+        Me.txtUltimoCaracter = New System.Windows.Forms.TextBox()
+        Me.txtPrimerCaracter = New System.Windows.Forms.TextBox()
+        Me.txtPosicionFija = New System.Windows.Forms.TextBox()
+        Me.txtPosicionB = New System.Windows.Forms.TextBox()
+        Me.txtMayusculas = New System.Windows.Forms.TextBox()
+        Me.txtMinusculas = New System.Windows.Forms.TextBox()
+        Me.txtBorrasEspacios = New System.Windows.Forms.TextBox()
+        Me.btnMostrar = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cadena"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Longitud"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(29, 117)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Ultimo Caracter"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(29, 155)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(79, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Primer Caracter"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(29, 192)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(54, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "3ro al 8vo"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(29, 233)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(110, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Posicion de la Letra B"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(29, 272)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(63, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Mayusculas"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(29, 314)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(60, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Minusculas"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(29, 354)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(82, 13)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Borras espacios"
+        '
+        'txtCadena
+        '
+        Me.txtCadena.Location = New System.Drawing.Point(152, 32)
+        Me.txtCadena.Name = "txtCadena"
+        Me.txtCadena.Size = New System.Drawing.Size(100, 20)
+        Me.txtCadena.TabIndex = 9
+        '
+        'txtLongitud
+        '
+        Me.txtLongitud.Location = New System.Drawing.Point(152, 72)
+        Me.txtLongitud.Name = "txtLongitud"
+        Me.txtLongitud.Size = New System.Drawing.Size(100, 20)
+        Me.txtLongitud.TabIndex = 10
+        '
+        'txtUltimoCaracter
+        '
+        Me.txtUltimoCaracter.Location = New System.Drawing.Point(152, 117)
+        Me.txtUltimoCaracter.Name = "txtUltimoCaracter"
+        Me.txtUltimoCaracter.Size = New System.Drawing.Size(100, 20)
+        Me.txtUltimoCaracter.TabIndex = 11
+        '
+        'txtPrimerCaracter
+        '
+        Me.txtPrimerCaracter.Location = New System.Drawing.Point(152, 155)
+        Me.txtPrimerCaracter.Name = "txtPrimerCaracter"
+        Me.txtPrimerCaracter.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrimerCaracter.TabIndex = 12
+        '
+        'txtPosicionFija
+        '
+        Me.txtPosicionFija.Location = New System.Drawing.Point(152, 192)
+        Me.txtPosicionFija.Name = "txtPosicionFija"
+        Me.txtPosicionFija.Size = New System.Drawing.Size(100, 20)
+        Me.txtPosicionFija.TabIndex = 13
+        '
+        'txtPosicionB
+        '
+        Me.txtPosicionB.Location = New System.Drawing.Point(152, 231)
+        Me.txtPosicionB.Name = "txtPosicionB"
+        Me.txtPosicionB.Size = New System.Drawing.Size(100, 20)
+        Me.txtPosicionB.TabIndex = 14
+        '
+        'txtMayusculas
+        '
+        Me.txtMayusculas.Location = New System.Drawing.Point(152, 272)
+        Me.txtMayusculas.Name = "txtMayusculas"
+        Me.txtMayusculas.Size = New System.Drawing.Size(100, 20)
+        Me.txtMayusculas.TabIndex = 15
+        '
+        'txtMinusculas
+        '
+        Me.txtMinusculas.Location = New System.Drawing.Point(152, 311)
+        Me.txtMinusculas.Name = "txtMinusculas"
+        Me.txtMinusculas.Size = New System.Drawing.Size(100, 20)
+        Me.txtMinusculas.TabIndex = 16
+        '
+        'txtBorrasEspacios
+        '
+        Me.txtBorrasEspacios.Location = New System.Drawing.Point(152, 351)
+        Me.txtBorrasEspacios.Name = "txtBorrasEspacios"
+        Me.txtBorrasEspacios.Size = New System.Drawing.Size(100, 20)
+        Me.txtBorrasEspacios.TabIndex = 17
+        '
+        'btnMostrar
+        '
+        Me.btnMostrar.Location = New System.Drawing.Point(266, 47)
+        Me.btnMostrar.Name = "btnMostrar"
+        Me.btnMostrar.Size = New System.Drawing.Size(75, 23)
+        Me.btnMostrar.TabIndex = 18
+        Me.btnMostrar.Text = "Mostrar"
+        Me.btnMostrar.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(391, 403)
+        Me.Controls.Add(Me.btnMostrar)
+        Me.Controls.Add(Me.txtBorrasEspacios)
+        Me.Controls.Add(Me.txtMinusculas)
+        Me.Controls.Add(Me.txtMayusculas)
+        Me.Controls.Add(Me.txtPosicionB)
+        Me.Controls.Add(Me.txtPosicionFija)
+        Me.Controls.Add(Me.txtPrimerCaracter)
+        Me.Controls.Add(Me.txtUltimoCaracter)
+        Me.Controls.Add(Me.txtLongitud)
+        Me.Controls.Add(Me.txtCadena)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Funciones String"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtCadena As TextBox
+    Friend WithEvents txtLongitud As TextBox
+    Friend WithEvents txtUltimoCaracter As TextBox
+    Friend WithEvents txtPrimerCaracter As TextBox
+    Friend WithEvents txtPosicionFija As TextBox
+    Friend WithEvents txtPosicionB As TextBox
+    Friend WithEvents txtMayusculas As TextBox
+    Friend WithEvents txtMinusculas As TextBox
+    Friend WithEvents txtBorrasEspacios As TextBox
+    Friend WithEvents btnMostrar As Button
 End Class
--- IPARCIAL/Trabajos de Clase/FuncionesString/Form1.vb
+++ IPARCIAL/Trabajos de Clase/FuncionesString/Form1.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Form1
-
+    Private Sub btnMostrar_Click(sender As Object, e As EventArgs) Handles btnMostrar.Click
+        Dim cadena As String
+        cadena = txtCadena.Text
+        'lenght retorna el Nº de posiciones de una cadena, incluyendo los espacios
+        txtLongitud.Text = cadena.Length
+        txtUltimoCaracter.Text = cadena.Substring(cadena.Length - 1, 1)
+        txtPrimerCaracter.Text = cadena.Substring(0, 1)
+        txtPosicionFija.Text = cadena.Substring(2, 7)
+        txtPosicionB.Text = cadena.IndexOf("B", 0) + 1
+        txtMayusculas.Text = cadena.ToUpper
+        txtMinusculas.Text = cadena.ToLower
+        txtBorrasEspacios.Text = cadena.Replace(" ", "")
+    End Sub
 End Class
